28e72beb48f9d7cc1a592038121d2bb26c61d03e
“Gpeter01” <240795083+Gpeter01@users.noreply.github.com>
2026-08-07 20:16:15 +0100

    Fix the inconsistencies in the width of the container

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ function setContainerSize() {
     const styleOfNine = getComputedStyle(nine);
     const newStyle = (parseFloat(styleOfNine.width) * 3) + (parseFloat(styleOfNumberpad.gap) * 2);
     numberpad.style.width = `${newStyle}px`;
+    const zero = document.querySelector('.zero');
+    const point = document.querySelector('.point');
+    const keyFeatureWidth = (newStyle - parseFloat(styleOfNumberpad.gap)) / 2;
+    zero.style.width = `${keyFeatureWidth}px`;
+    point.style.width = `${keyFeatureWidth}px`;
 }
 setContainerSize();
