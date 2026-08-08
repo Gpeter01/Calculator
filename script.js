bd702eb42943a10b08063090fa0f4f784f0b7fc8
“Gpeter01” <240795083+Gpeter01@users.noreply.github.com>
2026-08-08 08:22:51 +0100

    Enable the recieve of number input

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 function setContainerSize() {
     const nine = document.querySelector('.nine');
     const numberpad = document.querySelector('.number-pad');
@@ -12,3 +13,79 @@ function setContainerSize() {
     point.style.width = `${keyFeatureWidth}px`;
 }
 setContainerSize();
+
+const variables = {
+    'firstNumber' : '',
+    'operationSign' : '',
+    'currentNumber' : '',
+}
+const content = {
+    get displayContent() {
+        return variables.firstNumber + variables.operationSign + variables.currentNumber;
+    }
+}
+let currentMethod = '';
+let clickCount = 0;
+
+const operands = document.querySelector('.operands');
+operands.addEventListener('mousedown', iterateNumber);
+
+const special = document.querySelector('.special');
+special.addEventListener('click', executeSpecialKey);
+
+const calculatorOperations = document.querySelector('.calculator-operations');
+calculatorOperations.addEventListener('click', getOperation);
+
+function iterateNumber(event) {
+    const target = event.target;
+    const numbers = {
+        'nine': '9',
+        'eight': '8',
+        'seven': '7',
+        'six': '6',
+        'five': '5',
+        'four': '4',
+        'three': '3',
+        'two': '2',
+        'one': '1',
+        'zero': '0',
+        'point': '.'
+    }
+    function getNumber() {
+        for (const key in numbers) {
+            if (target.classList[0] == key) {
+                if (content.displayContent.length > 0) {
+                    const lastCharacter = content['displayContent'].charAt(content['displayContent'].length - 1);
+                    if (lastCharacter == ('÷' || '\u00D7' || '+' || '-')) {
+                        variables.currentNumber += numbers[key];
+                        return;
+                    } else { 
+                        variables.firstNumber += numbers[key];
+                        return;
+                    }
+                } else {
+                    variables.firstNumber += numbers[key];
+                }
+            }
+        }
+    }
+    getNumber();
+    const paraQuery = document.querySelector('.query');
+    const paraResult = document.querySelector('.para-result');
+    const styleOfParaQuery = getComputedStyle(paraQuery);
+    if (styleOfParaQuery.opacity == 0) {
+        paraQuery.style.opacity = 1;
+        paraResult.style.opacity = 1;
+    }
+    content.displayContent;
+    console.log(variables.firstNumber);
+    console.log(content.displayContent);
+    paraQuery.textContent = content.displayContent;
+    return;
+}
+function executeSpecialKey() {
+
+}
+function getOperation() {
+
+}
